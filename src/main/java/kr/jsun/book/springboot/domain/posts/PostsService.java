0df39f4f0ce4b9c61a7c21850f92d9510b7ae7a3
JONGSUNMOON <whdtjstm@gmail.com>
2020-09-01 15:30:36 +0900

    fixed untracked files

--- src/main/java/kr/jsun/book/springboot/domain/posts/PostsService.java
+++ src/main/java/kr/jsun/book/springboot/domain/posts/PostsService.java
@@ -1,11 +1,15 @@
 package kr.jsun.book.springboot.domain.posts;
 
+import kr.jsun.book.springboot.web.dto.PostsListResponseDto;
 import kr.jsun.book.springboot.web.dto.PostsResponseDto;
 import kr.jsun.book.springboot.web.dto.PostsSaveRequestDto;
 import kr.jsun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +33,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Long delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+
+        return id;
     }
 }
